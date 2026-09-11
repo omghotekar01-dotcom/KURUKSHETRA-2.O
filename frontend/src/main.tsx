41bf522d7df9024e7efd786271de35a7fd07a9f8
Omiiii <omghotekar01@gmail.com>
2026-09-11 14:09:55 +0530

    feat(ui): wire persistent theme controls

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import ThemeToggle from './ThemeToggle'
 import './styles.css'
 import './workflow.css'
+import './theme.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <ThemeToggle />
   </React.StrictMode>,
 )
