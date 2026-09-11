2a3ecd84c9d5174e725bb0e02f8e1ac29b705bce
Omiiii <omghotekar01@gmail.com>
2026-09-11 14:49:50 +0530

    feat: route evidence lab screen

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import RepositoryEvidencePage from './RepositoryEvidencePage'
 import ThemeToggle from './ThemeToggle'
 import './styles.css'
 import './workflow.css'
 import './theme.css'
 import './repository-theme.css'
+import './evidence-page.css'
+
+const path = window.location.pathname.replace(/\/+$/, '') || '/'
+const screen = path === '/evidence' ? <RepositoryEvidencePage /> : <App />
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {screen}
     <ThemeToggle />
   </React.StrictMode>,
 )
